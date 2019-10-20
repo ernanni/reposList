b40074400b19483337aebe92c9f3615dcf03ab4f
Ernanni <ernannicf@gmail.com>
2019-10-20 17:49:29 -0300

    feat(reposList): first commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+const App: () => React$Node = () => {
+  return (
+    <>
+      <Text>Hello World</Text>
+    </>
+  );
+};
+
+export default App;
