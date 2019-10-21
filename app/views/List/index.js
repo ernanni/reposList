6f7bc08f3f60ba50a1a0edef165cc34e93389717
Ernanni <ernannicf@gmail.com>
2019-10-20 23:14:56 -0300

    part 1: axios request

--- app/views/List/index.js
+++ app/views/List/index.js
@@ -1,23 +1,49 @@
 import React, { Component } from 'react'
 import { View, Text } from 'react-native'
+import axios from 'axios'
  
 class List extends Component {
  
   state = {
-    username: JSON.stringify(this.props.navigation.getParam('name'))
+    username: '',
+    reposList: []
   }
   constructor(props){
     super(props)
   }
+
+  componentDidMount() {
+    this.setState({username: this.props.navigation.getParam('name', 'ernanni')})
+    console.log(this.state.username)
+    
+    axios.get(`https://api.github.com/users/ernanni/repos`)
+      .then(response => {
+        console.log('success ',response)
+        this.setState({reposList: response})
+      })
+      .catch(error => {console.log('error ', error.response.data)})
+  }
+
+  mountList() {
+    return this.state.reposList.map(item, index => {
+      return (
+        <View>
+          <Text>{item.name}</Text>
+          <Text>{item.full_name}</Text>
+        </View>
+      )
+    })
+  }
+
   render() {
     return (
       <View>
-        <Text>{this.state.username}</Text>
+        {this.state.reposList}
       </View>
     )
   }
 }
- 
+
 List.navigationOptions = {
   title: 'List'
 }
